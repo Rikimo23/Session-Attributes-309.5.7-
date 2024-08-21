d8d27ad626c3f0c9b7a0fddc4463a9b84dc03d5a
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-08-21 16:08:31 -0400

    finished

---
+++ src/main/java/com/example/Session/Attributes/Session/Attributes/SessionAttributesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Session.Attributes.Session.Attributes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionAttributesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionAttributesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Session/Attributes/Session/Attributes/SessionAttributesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Session.Attributes.Session.Attributes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionAttributesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
